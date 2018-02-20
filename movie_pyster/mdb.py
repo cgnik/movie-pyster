4401eae32b56fbbda8370bec3733d3d125090409
Christopher King <cking@enmobilellc.com>
2018-02-19 19:55:31 -0500

    config and search

--- movie_pyster/mdb.py
+++ movie_pyster/mdb.py
@@ -1,8 +1,16 @@
 import tmdbsimple
+import fuzzywuzzy
 
-# with open('themoviedb-key.txt', 'rb')as keyfile:
-#     tmdb.API_KEY = keyfile.read()
+with open('themoviedb-key.txt', 'rb')as keyfile:
+    tmdbsimple.API_KEY = keyfile.read()
+
+
+def base_url():
+    return tmdbsimple.Configuration().base_url
 
 
 def search(name):
     return tmdbsimple.Search().movie(query=name)['results']
+
+def best_movie_match(title, titles):
+    return 0
--- movie_pyster/mdb_test.py
+++ movie_pyster/mdb_test.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 
 from movie_pyster import mdb
 
@@ -7,10 +7,19 @@ import tmdbsimple
 
 
 class TestMdb(TestCase):
+
+    @patch('tmdbsimple.Configuration')
+    def test_configuration(self, c):
+        expected = "urk/nok"
+        type(c()).base_url = PropertyMock(return_value=expected)
+        self.assertEqual(mdb.base_url(), expected)
+
     @patch('tmdbsimple.Search')
     def test_search(self, s):
         expected = ['blah']
         s().movie = Mock(return_value={'results': expected})
-        result = mdb.search("Aliens")
-        self.assertEquals(result, expected)
+        self.assertEquals(mdb.search("Aliens"), expected)
         s().movie.assert_called_with(query="Aliens")
+
+    def test_best_title_match(self):
+        pass
