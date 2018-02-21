e4f331ad5626081406cede6c1e046c837b54b5fb
Christopher King <cking@enmobilellc.com>
2018-02-20 20:08:33 -0500

    ignored cache; added some command line stuff

--- movie_pyster/main.py
+++ movie_pyster/main.py
@@ -1,6 +1,14 @@
+from sys import argv
+from os import getcwd
+
+from movie_pyster.util import movie_files, filename
 from movie_pyster.mdb import best_movie_match
 
 if __name__ == '__main__':
-    print("Begin")
-    print(best_movie_match('Aliens'))
-    print("End")
+    keyfile = argv[2]
+    dirs = argv[:2] if len(argv) > 3 else [getcwd()]
+    for dir in dirs:
+        moviefiles = movie_files(dir)
+        for moviefile in moviefiles:
+            print("{} matched: {}".format(moviefile, best_movie_match(filename(moviefile))))
+    print("Done.")
