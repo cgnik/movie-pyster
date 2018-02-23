a362df210457f1cd5c6efe493e2b5304ff2f5c15
Christopher King <cking@enmobilellc.com>
2018-02-22 21:25:11 -0500

    logging refactor

--- movie_pyster/main.py
+++ movie_pyster/main.py
@@ -14,11 +14,14 @@ from movie_pyster.mdb import best_movie_match, base_url
 class MoviePyster:
 
     def __init__(self):
-        self.log = logging.getLogger("movie_pyster")
-        self.log.setLevel(logging.INFO)
+        self.logger = logging.getLogger("movie_pyster")
+        self.logger.setLevel(logging.INFO)
         con = logging.StreamHandler()
         con.setLevel(logging.INFO)
-        self.log.addHandler(con)
+        self.logger.addHandler(con)
+
+    def log(self, message):
+        self.logger.info(message)
 
     def process(self, dirs):
         image_base_url = base_url()
@@ -27,13 +30,12 @@ class MoviePyster:
             for moviefile in movie_files(dir):
                 count = count + 1
                 self.update_movie(image_base_url, moviefile)
-        self.log.info("Processed {} movie files.".format(count))
+        self.log("\nDone processing {} movie files from {} directories".format(count, len(dirs)))
 
     def fetch_movie_image(self, base_url, name, poster_path):
         if poster_path:
             fetched = http_fetch("{}/{}{}".format(base_url, 'w780', poster_path), name)
-        else:
-            fetched = "No match for title '{}'".format(name)
+            self.log("Fetched image for {}".format(name))
         return fetched
 
     def update_movie(self, image_base_url, moviefile):
@@ -41,9 +43,8 @@ class MoviePyster:
         best = best_movie_match(name)
         if best:
             fetched_image = self.fetch_movie_image(image_base_url, name, best['poster_path'])
-            self.log.info("Fetched image {} for movie '{}'".format(fetched_image, name))
         else:
-            self.log.warn("Unable to match {}".format(name))
+            self.log("No match for movie {}".format(moviefile))
 
 
 if __name__ == '__main__':
